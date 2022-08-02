e03a6ce04f69544eea2f05206f98f1b3edeed920
jaejin96 <songjj77@naver.com>
2022-08-02 17:57:54 +0900

    page 옮기기

---
+++ src/components/teamPage/teamTop.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="team-container">
+    <div class="temp">임시텍스트</div>
+  </div>
+</template>
+
+<style lang="scss">
+.team-container {
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  background-image: url("../../assets/team-1.jpeg");
+  background-size: cover;
+  .temp {
+    display: flex;
+    margin: 0 auto;
+    top: 100px;
+    color: white;
+    font-weight: 700;
+    align-items: center;
+    font-size: 100px;
+  }
+}
+</style>
