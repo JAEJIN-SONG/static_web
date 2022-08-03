fcdf02e743bd61288a6557a547befebe0385970b
jaejin96 <songjj77@naver.com>
2022-08-03 14:13:18 +0900

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,9 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
+    // alias: {
+    //   "@": fileURLToPath(new URL("./src", import.meta.url)),
+    // },
+    alias: [{ find: "@", replacement: "/src" }],
   },
 });
